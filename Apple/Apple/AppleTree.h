bbe5daf7873b5693ca45c52d2e818a7c8b8e1654
fpmisk <fpmisc2015@gmail.com>
2015-09-06 22:57:25 +0300

    Update AppleTree.h

--- Apple/Apple/AppleTree.h
+++ Apple/Apple/AppleTree.h
@@ -1,14 +1,25 @@
 //
 //  AppleTree.h
-//  Apple
+//  lab1
 //
-//  Created by fpmi on 03.09.15.
-//  Copyright (c) 2015 fpmi. All rights reserved.
+//  Created by Admin on 06.09.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import "Apple.h"
+#import "List.h"
 
 @interface AppleTree : NSObject
 
+@property NSInteger *age;
+@property NSMutableArray *arrayOfApple;
+@property NSMutableArray *arrayOfList;
+
+-(bool) dropApple: (Apple *) apple;
+-(bool) dropList: (List *) list;
+-(void) grow;
+-(void) shake;
+-(id) initWithAge: (NSInteger *) age;
 
 @end
