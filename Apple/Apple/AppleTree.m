3f386646e7b90a02f9f4a585b385d3b2f4839d10
fpmisk <fpmisc2015@gmail.com>
2015-09-06 22:58:27 +0300

    Update AppleTree.m

--- Apple/Apple/AppleTree.m
+++ Apple/Apple/AppleTree.m
@@ -1,13 +1,48 @@
 //
 //  AppleTree.m
-//  Apple
+//  lab1
 //
-//  Created by fpmi on 03.09.15.
-//  Copyright (c) 2015 fpmi. All rights reserved.
+//  Created by Admin on 03.09.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
 //
 
 #import "AppleTree.h"
 
 @implementation AppleTree
+
+@synthesize arrayOfApple = _arrayOfApple;
+@synthesize arrayOfList = _arrayOfList;
+@synthesize age = _age;
+
+-(bool) dropApple: (Apple *) apple
+{
+    return false;
+}
+-(bool) dropList: (List *) list
+{
+    return false;
+}
+-(void) grow
+{
+    
+}
+-(void) shake
+{
+    
+}
+-(id) initWithAge:(NSInteger *)age
+{
+    self = [super init];
+    if(self)
+    {
+        [self setAge:age];
+        NSMutableArray * temp = [[NSMutableArray alloc] init];
+        [self setArrayOfApple:temp];
+        temp = [[NSMutableArray alloc] init];
+        [self setArrayOfList:temp];
+        NSLog(@"AppleTree was created");
+    }
+    return self;
+}
 
 @end
