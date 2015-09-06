01bc4064850c7bc8bf757967af2ce85f0afc7b10
fpmisk <fpmisc2015@gmail.com>
2015-09-06 22:54:51 +0300

    Update List.h

--- Apple/Apple/List.h
+++ Apple/Apple/List.h
@@ -1,13 +1,27 @@
 //
 //  List.h
-//  Apple
+//  lab1
 //
-//  Created by fpmi on 03.09.15.
-//  Copyright (c) 2015 fpmi. All rights reserved.
+//  Created by Admin on 03.09.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
+typedef enum _ListColor
+{
+    red,
+    green,
+    yellow
+} ListColor;
+
 @interface List : NSObject
+
+@property ListColor currentColor;
+@property BOOL isHang;
+-(id) init;
+-(BOOL) drop;
+-(void) grow;
+
 
 @end
