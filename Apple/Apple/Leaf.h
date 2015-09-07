1bc200d469a63113f42554808c7abb6aa7c9b45a
fpmisk <fpmisc2015@gmail.com>
2015-09-07 21:35:03 +0300

    Update Leaf.h

--- Apple/Apple/Leaf.h
+++ Apple/Apple/Leaf.h
@@ -1,19 +1,40 @@
-#import <Foundation/Foundation.h>
+#import "Leaf.h"
 
-typedef enum _LeafColor
-{
-    leafOfRed,
-    leafOfGreen,
-    leafOfYellow
-}LeafColor;
+@implementation Leaf
 
-@interface Leaf : NSObject
+@synthesize currentColor = _currentColor;
+@synthesize isHang = _isHang;
 
-@property LeafColor currentColor;
-@property BOOL isHang;
--(id) init;
--(BOOL) drop;
--(void) grow;
+-(BOOL) drop {
+    
+    if ([self isHang] == true) {
+        [self setIsHang:false];
+        NSLog(@"Leaf drop from tree");
+    }
+    return [self isHang];
+}
 
-
+-(void) grow {
+    if([self currentColor] == leafOfRed){
+        NSLog(@"Leaf is red");
+    }
+    else if([self currentColor] == leafOfGreen){
+        [self setCurrentColor: (LeafColor) leafOfYellow];
+        NSLog(@"Leaf is yellow");
+    }
+    else if([self currentColor] == leafOfYellow){
+        [self setCurrentColor: (LeafColor) leafOfRed];
+        NSLog(@"Leaf is red");
+    }
+}
+-(id) init{
+    self = [super init];
+    if(self)
+    {
+        [self setCurrentColor: (LeafColor) leafOfGreen];
+        [self setIsHang: true];
+        NSLog(@"Leaf was created");
+    }
+    return self;
+}
 @end
