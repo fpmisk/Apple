b020de5aea32e3aff33cab86117291fb5b8dccf7
fpmisk <fpmisc2015@gmail.com>
2015-09-18 21:53:37 +0300

    Repo re-init

---
+++ Apple/Apple/Basket.h
@@ -0,0 +1,20 @@
+//
+//  Basket.h
+//  Apple
+//
+//  Created by fpmi on 10.09.15.
+//  Copyright (c) 2015 fpmi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Fruit.h"
+#import "BasketIterator.h"
+
+@interface Basket : NSObject
+
+@property NSMutableArray * basketOfFruit;
+
+-(id) initWithArray: (NSMutableArray *) array;
+-(BasketIterator *) iterator;
+
+@end
